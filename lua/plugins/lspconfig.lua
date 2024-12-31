0f957c0c31c793cd0ca9e65f0f4be37acbc220fe
Magnus Lind <sungam999@gmail.com>
2024-12-31 12:03:37 +0100

    had a brain fart and didn't push plugins folder

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,24 @@
+return {
+    {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+	  {
+	    "folke/lazydev.nvim",
+	    ft = "lua", -- only load on lua files
+	    opts = {
+	      library = {
+		-- See the configuration section for more details
+		-- Load luvit types when the `vim.uv` word is found
+		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+	      },
+	    },
+	  },
+	},
+	config = function()
+	    local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+	    require'lspconfig'.lua_ls.setup{ capabilities = capabilities }
+	    require'lspconfig'.clangd.setup{ capabilities = capabilities }
+	end
+    }
+}
